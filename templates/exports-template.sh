a6c275257319aa91cfcd888d2fd7fee3a3701b58
lin-crypto <dev.thomas128@gmail.com>
2023-10-24 17:27:10 -0400

    update template

--- templates/exports-template.sh
+++ templates/exports-template.sh
@@ -56,9 +56,7 @@ export MSP_PATH=/opt/home/admin-msp
 export MSP=$MEMBERID
 export ORDERER=$ORDERINGSERVICEENDPOINT
 export PEER=$PEERSERVICEENDPOINT
-export CHANNEL=mychannel
 export CAFILE=/opt/home/managedblockchain-tls-chain.pem
-export CHAINCODENAME=mycc
 export CHAINCODEVERSION=v0
 export CHAINCODEDIR=github.com/chaincode_example02/go
 EOF
